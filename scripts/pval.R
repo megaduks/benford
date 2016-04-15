8044812af2e7d7e3f71ef51bf56b1ef09dcaa8bd
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2016-04-15 20:29:06 +0000

    Correct experiment with tests' p-values

---
+++ scripts/pval.R
@@ -0,0 +1,88 @@
+library(BenfordTests)
+library(benford.analysis)
+library(dplyr)
+library(ggplot2)
+
+# size of the sample
+n <- 10000
+
+# pure samples of Benford and Normal distributions
+benford.sample <- rbenf(n)
+normal.sample <- rnorm(n)
+
+# ratio of Benford's distribution contamination with Normal samples
+ratio <- seq(0, 1, by = 0.05)
+
+results <- data.frame(i = numeric(), test = character(), test.number = character(), pval = numeric(), statistic = numeric()) 
+
+for (i in ratio) {
+  for (j in 1:50) {
+    
+    data.sample <- c( sample(benford.sample, n*i), sample(normal.sample, n*(1-i)))
+    
+    t <- chisq.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t1", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- edist.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t2", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- jointdigit.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t3", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- jpsq.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t4", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- ks.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t5", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- mdist.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t6", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- usq.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t7", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- meandigit.benftest(data.sample)
+    d <- data.frame(i, test = t$method, test.number = "t8", pval = t$p.value, statistic = t$statistic)
+    results <- rbind(results, d)
+    
+    t <- benford(data.sample, number.of.digits = 1)
+    d <- data.frame(i, test = "Mean Absolute Deviation", test.number = "t9", pval = NA, statistic = t$MAD)
+    results <- rbind(results, d)
+    d <- data.frame(i, test = "Distortion Factor", test.number = "t10", pval = NA, statistic = t$distortion.factor)
+    results <- rbind(results, d)
+    d <- data.frame(i, test = t$stats$mantissa.arc.test$method, test.number = "t11", pval = t$stats$mantissa.arc.test$p.value, statistic = t$stats$mantissa.arc.test$statistic)
+    results <- rbind(results, d)
+  }
+}
+
+row.names(results) <- NULL
+
+avg.results <- results %>%
+  group_by(i, test, test.number) %>%
+  summarize(avg.pval = mean(pval), avg.stat = mean(statistic))
+
+avg.results <- avg.results %>%
+  mutate( flag = ifelse(avg.pval>=0.05 & test.number=='t1', 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t2') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t3') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t4') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t5') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t6') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t7') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t8') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.stat<=0.0018 & test.number=='t9') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((abs((avg.stat/100)/(0.638/sqrt(1000))) <= 1.96 & test.number=='t10') | flag==1, 1, 0)) %>%
+  mutate( flag = ifelse((avg.pval>=0.05 & test.number=='t11') | flag==1, 1, 0))
+
+
+avg.results %>% select(i, test.number, flag, avg.pval, avg.stat) %>% filter(flag == 1)
+
+ggplot(avg.results[!(avg.results$test.number %in% c('t9','t10')), ], aes(x = i, y = avg.pval, colour = test.number)) + 
+  geom_line() + geom_point()
